66fd901fde0e97a03ab307c0bb0aa373d8554c4d
KacperKi <kacperxawer.123@gmail.com>
2023-03-13 00:13:19 +0100

    GUI with JFile and Update JTable

--- src/Main.java
+++ src/Main.java
@@ -1,169 +1,159 @@
 import javax.swing.*;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.xml.crypto.Data;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-
-import com.opencsv.CSVReader;
-import de.vandermeer.asciitable.AsciiTable;
 
 public class Main {
-    static String pathToFile;
-    static ArrayList<String> nameOfColumnsFromFile = new ArrayList<String>(){
-        {
-            add("nr.");add("nazwa producenta"); add("przekątna ekranu"); add("rozdzielczość ekranu"); add("rodzaj powierzchni ekranu");
-            add("ekran dotykowy"); add("nazwa procesora"); add("liczba rdzeni fizycznych"); add("prędkość taktowania MHz");
-            add("pamięć RAM"); add("pojemność dysku"); add("rodzaj dysku"); add("nazwa układu graficznego");
-            add("pamięć układu graficznego"); add("nazwa systemu operacyjnego"); add("rodzaj napędu fizycznego");
-        }
-    };
-    static ArrayList<String> rowsFromFile = new ArrayList<>();
-
-    static JFrame mainFrame;
 
     public static void main(String[] args){
-        CreateFrameAndShow();
-
-        SelectAndCheckFileWithData();
-        ReadDataFromFile();
-        PrintHeaderOfTable();
-        ColumnNameBuildAndPrint();
-        ConvertAndPrintDataFromArray();
+        new Main();
     }
+    String pathToFile;
+    ArrayList<String> nameOfColumnsFromFile = new ArrayList<String>() {
+        {
+            add("nazwa producenta");
+            add("przekątna ekranu");
+            add("rozdzielczość ekranu");
+            add("rodzaj powierzchni ekranu");
+            add("ekran dotykowy");
+            add("nazwa procesora");
+            add("liczba rdzeni fizycznych");
+            add("prędkość taktowania MHz");
+            add("pamięć RAM");
+            add("pojemność dysku");
+            add("rodzaj dysku");
+            add("nazwa układu graficznego");
+            add("pamięć układu graficznego");
+            add("nazwa systemu operacyjnego");
+            add("rodzaj napędu fizycznego");
+        }
+    };
+    ArrayList<ArrayList<String>> dataToTable;
+    JFrame mainFrame;
+    JButton ImportButton, SaveButtonData;
+    JTable tableWithData;
+    JScrollPane scrollPane;
+    Object[][] dataToSave;
+    DefaultTableModel model;
 
+    public Main() {
+      CreateFrame();
+      CreateListener();
+    }
+    void CreateFrame(){
+        mainFrame = new JFrame("Integracja Systemów - Kacper Kisielewski");
 
-    public static void CreateFrameAndShow(){
-        mainFrame = new JFrame("Kacper Kisielewski - LAB 2");
+        ImportButton = new JButton("Import data from TXT file");
+        SaveButtonData = new JButton("Export data to TXT file");
 
-        JButton ImportButton = new JButton("Import data from TXT file");
-        JButton SaveButtonData = new JButton("Export data to TXT file");
-
-        ImportButton.setBounds(50,100, 95, 30); SaveButtonData.setBounds(100, 100, 95,30);
+        ImportButton.setBounds(10,5, 200, 30); SaveButtonData.setBounds(220, 5, 200,30);
         mainFrame.add(ImportButton); mainFrame.add(SaveButtonData);
 
-        mainFrame.setSize(500, 400);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setSize(450, 80);
         mainFrame.setLayout(null);
         mainFrame.setVisible(true);
-
-        while(true) {
-            //do nothing
-        }
     }
-    public static void SelectAndCheckFileWithData(){
-        boolean FileExists = false;
-        while(!FileExists) {
-            System.out.println("Insert Path to File: ");
-            Scanner consolePath = new Scanner(System.in);
-            pathToFile = consolePath.nextLine();
-
-            File f = new File(pathToFile);
-            if (f.exists() && !f.isDirectory()) {
-                System.out.println("File exists - OK!");
-                break;
+    void CreateListener(){
+        ImportButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ImportButtonFunction();
             }
-            else System.out.println("File doesn't exists!");
+        });
+        SaveButtonData.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //w
+            }
+        });
+    }
+    void ImportButtonFunction(){
+        PrintInformationAndSelectFileWithData();    //Select correct TXT file
+        ReadDataFromFile();                         //Import all data from txt file
+        if(scrollPane == null) ShowTable();
+        else {
+            this.model= new DefaultTableModel(ConvertDataToObject(dataToTable), nameOfColumnsFromFile.toArray());
+            this.tableWithData.setModel(model);
         }
     }
-    public static void ReadDataFromFile() {
+    void PrintInformationAndSelectFileWithData(){
+        JFileChooser chooser = new JFileChooser(pathToFile);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Select only txt files", "txt");
+        chooser.setFileFilter(filter);
+        int returnVal = chooser.showOpenDialog(mainFrame);
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+            System.out.println("You chose to open this file: " + chooser.getSelectedFile());
+            pathToFile = String.valueOf(chooser.getSelectedFile());
+        }
+    }
+    void ReadDataFromFile() {
+        dataToTable = new ArrayList<>();
+        if(dataToTable.size() != 0) dataToTable.clear();
+
         String line;
+        System.out.println("PATH: " + pathToFile);
         try (BufferedReader br = new BufferedReader(new FileReader(pathToFile))) {
             while ((line = br.readLine()) != null) {
-                rowsFromFile.add(line);
+                   ArrayList<String> results = new ArrayList<>();
+                   results.addAll(Arrays.asList(line.split(";")));
+                   while(results.size() < nameOfColumnsFromFile.size()){
+                       results.add("brak");
+                   }
+                   dataToTable.add(results);
             }
         } catch (Exception e) {
             System.out.println(e);
         }
-
     }
-    public static void PrintHeaderOfTable(){
-        System.out.printf("--------------------------------%n");
-        System.out.printf("         Table with data        %n");
-        System.out.printf("--------------------------------%n");
-    }
-    public static void ColumnNameBuildAndPrint(){
-        AsciiTable at = new AsciiTable();
-        at.addRule();
-        at.addRow(nameOfColumnsFromFile);
-        at.addRule();
-        System.out.println(at.render(200));
-    }
-    public static void ConvertAndPrintDataFromArray(){
-        ArrayList<String> elementsOfTable = new ArrayList<>();
-        AsciiTable at = new AsciiTable();
-        ArrayList<String> nameOfProducent = new ArrayList<>();
-        ArrayList<Integer> counterOfProducent = new ArrayList<>();
+    Object[][] ConvertDataToObject(ArrayList<ArrayList<String>> data){
+        int row = data.size();
+        int column = data.get(0).size();
+        Object[][] result = new Object[row][column];
 
-        int numberOfRow = 1;
-
-        for(String row: rowsFromFile){
-            String[] result = row.split(";");
-            elementsOfTable.add(String.valueOf(numberOfRow));
-
-            for (int x=0; x<result.length; x++){
-                String tmp = result[x];
-                if(!tmp.equals("")) elementsOfTable.add(result[x]);
-                else elementsOfTable.add("brak");
-
+        int i = 0, j = 0;
+        for(ArrayList<String> t: data){
+            j = 0;
+            for(String el: t){
+                if(el.equals("")) result[i][j] = "brak";
+                else result[i][j] = el;
+                j++;
             }
-
-            int position =0; int counter = 1;
-            if(nameOfProducent.contains(result[0])) {
-                position = nameOfProducent.indexOf(result[0]);
-                counter = counterOfProducent.get(position);
-                counter++;
-                counterOfProducent.set(position, counter);
-            }
-            else{
-                nameOfProducent.add(result[0]);
-                counterOfProducent.add(1);
-            }
-            if(elementsOfTable.size()==14) {
-                elementsOfTable.add("brak");
-                elementsOfTable.add("brak");
-            }
-            if(elementsOfTable.size()==15) elementsOfTable.add("brak");
-
-            at.addRow(elementsOfTable);
-            at.addRule();
-            elementsOfTable.clear();
-            numberOfRow++;
-
+            i++;
         }
-        System.out.println(at.render(200));
-
-        System.out.println("Tabela danych");
-        AsciiTable a = new AsciiTable();
-        a.addRule();
-        for(String t: nameOfProducent) {
-
-            a.addRow(new ArrayList<>(Arrays.asList(t, counterOfProducent.get(nameOfProducent.indexOf(t)))));
-            a.addRule();
-        }
-        System.out.println(a.render(30));
+        return result;
     }
-//    D:\Studia\Magisterskie\3 semestr\L_IntegracjaSystemow\katalog.txt
+    void ShowTable(){
+        this.model = new DefaultTableModel(ConvertDataToObject(dataToTable), nameOfColumnsFromFile.toArray());
+        this.tableWithData = new JTable(model);
+        this.scrollPane = new JScrollPane(tableWithData, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPane.setEnabled(true);
+        scrollPane.setBounds(10, 45, 1700, 250);
+        scrollPane.setVisible(true);
+
+        this.mainFrame.add(scrollPane);
+        mainFrame.setSize(1720,340);
+        mainFrame.invalidate();
+        mainFrame.validate();
+        mainFrame.repaint();
+
+        tableWithData.getModel().addTableModelListener(new TableModelListener() {
+            @Override
+            public void tableChanged(TableModelEvent e) {
+//                dataToSave = tableWithData.getModel().
+                System.out.println("It was changed col: " + e.getColumn() + " row: " + e.getLastRow()
+                        + " val: " + tableWithData.getModel().getValueAt(e.getFirstRow(), e.getColumn()));
+            }
+        });
+    }
 
 }
-
-
-
-//    Path to file with data
-//    static File pathToFile;
-//    Function with gui to select txt file
-//    public static void PrintInformationAndSelectFileWithData(){
-//        System.out.println("Laboratorium nr. 1!");
-//        JFileChooser chooser = new JFileChooser(pathToFile);
-//        FileNameExtensionFilter filter = new FileNameExtensionFilter("Select only txt files", "txt");
-//        chooser.setFileFilter(filter);
-//        int returnVal = chooser.showOpenDialog(null);
-//        if(returnVal == JFileChooser.APPROVE_OPTION) {
-//            System.out.println("You chose to open this file: " + chooser.getSelectedFile().getName());
-//        }
-//        pathToFile = chooser.getSelectedFile();
-//    }
